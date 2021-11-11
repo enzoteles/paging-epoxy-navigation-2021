002094577ea8461dbad61bafea59fc0f9eb0659c
Enzo <enzo.carvalho.teles@gmail.com>
2021-11-11 16:06:42 -0300

    inserindo o progress na lista

--- app/src/main/java/com/example/koroutinesmvvm/presentation/controller/list/CharacterListEpoxyController.kt
+++ app/src/main/java/com/example/koroutinesmvvm/presentation/controller/list/CharacterListEpoxyController.kt
@@ -3,6 +3,7 @@ package com.example.koroutinesmvvm.presentation.controller.list
 import com.airbnb.epoxy.EpoxyModel
 import com.airbnb.epoxy.paging.PagedListEpoxyController
 import com.example.koroutinesmvvm.R
+import com.example.koroutinesmvvm.common.LoadingEpoxyModel
 import com.example.koroutinesmvvm.databinding.ActivityCharacterListItemBinding
 import com.example.koroutinesmvvm.domain.model.GetCharacterByIdMapper
 import com.example.koroutinesmvvm.presentation.epoxy.ViewBindingKotlinModel
@@ -15,6 +16,14 @@ class CharacterListEpoxyController: PagedListEpoxyController<GetCharacterByIdMap
         item: GetCharacterByIdMapper?
     ): EpoxyModel<*> {
         return CharacterGridItemEpoxyModel(item?.image ?: "", item?.name ?: "").id(item?.id)
+    }
+
+    override fun addModels(models: List<EpoxyModel<*>>) {
+        if(models.isEmpty()){
+            LoadingEpoxyModel().id("loading").addTo(this)
+            return
+        }
+        super.addModels(models)
     }
 
     data class CharacterGridItemEpoxyModel(
